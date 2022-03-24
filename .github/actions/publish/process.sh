3459373868596299b319d70eb12247026bb1d852
Hugh Cayless <philomousos@gmail.com>
2022-03-24 13:11:03 -0400

    Adding action.

---
+++ .github/actions/publish/process.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILES=$(git diff --name-only $BEFORE..$AFTER | grep "sources/.*\.xml")
+
+convert() {
+  echo "Converting $1"
+  DIR=$(basename "$1" | sed 's/\.xml//')
+  if [ ! -d "editionscs/$DIR" ]; then
+    mkdir "editions/$DIR"
+  fi
+  echo "Output to editions/$DIR/index.md"
+  saxon -s:"$1" -xsl:xslt/publish.xsl -o:"docs/$DIR/index.md"
+}
+
+for f in $FILES
+do
+  echo "Processing file $f"
+  convert "$f"
+done  
