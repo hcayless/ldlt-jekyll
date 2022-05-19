1754fd44fbe21228fad210cea79507fb31feb5cd
Hugh Cayless <philomousos@gmail.com>
2022-05-19 17:24:44 -0400

    Updated action to put Jekyll files in _editions/.

--- .github/actions/publish/process.sh
+++ .github/actions/publish/process.sh
@@ -5,11 +5,11 @@ FILES=$(git diff --name-only $BEFORE..$AFTER | grep "sources/.*\.xml")
 convert() {
   echo "Converting $1"
   DIR=$(basename "$1" | sed 's/\.xml//' | tr "[:upper:]" "[:lower:]")
-  if [ ! -d "editionscs/$DIR" ]; then
-    mkdir "editions/$DIR"
+  if [ ! -d "_editions/$DIR" ]; then
+    mkdir "_editions/$DIR"
   fi
-  echo "Output to editions/$DIR/index.md"
-  saxon -s:"$1" -xsl:xslt/publish.xsl -o:"docs/$DIR/index.md"
+  echo "Output to _editions/$DIR/index.md"
+  saxon -s:"$1" -xsl:xslt/publish.xsl -o:"_editions/$DIR/index.md"
 }
 
 for f in $FILES
